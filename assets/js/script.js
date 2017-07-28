f0c01ca4f78cfd55b296c35ac2a5b4098ef79d14
Mai Hoang <mnhoang.80@gmail.com>
2017-07-28 15:59:26 -0700

    Add imgs/logos & about section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,52 @@
+function goHome (){
+    var home = document.getElementById('hero');
+    var allSections = document.getElementsByTagName('section');
+    var boxes = document.getElementsByClassName('animated-boxes');
+    console.log("ALL: ", typeof allSections);
+
+    home.style.display = 'block';
+
+    for (var i = 1; i < allSections.length; i++){
+        allSections[i].style.display = 'none';
+        if(boxes[i - 1].classList.contains('expand-box')){
+            boxes[i-1].classList.remove('expand-box');
+            boxes[i-1].classList.add('collapse-box');
+        }
+            
+    }
+
+}
+
 function toggleClass (element){
 
-	var boxes = document.getElementsByClassName('color-box');
+	var boxes = document.getElementsByClassName('animated-boxes');
 	var thisBox = element.childNodes[1];
-
+    
 	for(var i = 0; i < boxes.length; i++){
 		if(boxes[i].classList.contains('expand-box')){
 			boxes[i].classList.remove('expand-box');
 			boxes[i].classList.add('collapse-box');
 		}
 	}
-
+    
 	thisBox.classList.toggle('expand-box');
-	
+    showSection(thisBox);
 }
+
+function showSection(box){
+
+    var sections = document.getElementsByTagName('section');
+
+    for(var s = 0; s < sections.length; s++){
+        if(box.dataset.section !== sections[s].id){
+            sections[s].style.display = 'none';
+        }
+        if(box.dataset.section === sections[s].id){
+            sections[s].style.display = 'block';
+        }
+    }
+}
+
 
 var wavesFore = new SiriWave({
   width: 2000,
